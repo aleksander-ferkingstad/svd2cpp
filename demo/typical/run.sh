d80cba92b789e254919b16bdd3809c6747a0a992
Aleksander Ferkingstad <aleksander@alphatek.no>
2025-04-08 20:24:17 +0200

    add a demo to showcase a more modern approach

--- demo/typical/run.sh
+++ demo/typical/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#meson setup build --cross-file cross/stm32f767zi.ini --reconfigure --wipe
+meson setup build --cross-file cross/stm32f767zi.ini --reconfigure --wipe
 ninja -C build
 arm-none-eabi-objcopy -O ihex build/demo.elf build/demo.hex
 st-flash --reset --format=ihex write build/demo.hex
