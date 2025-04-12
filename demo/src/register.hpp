9c664fb63e11064e73ca1775c10b68ea0f53362e
Aleksander Ferkingstad <Aleksander@alphatek.no>
2025-04-12 21:47:36 +0200

    only do reinterpret cast on startup

--- demo/src/register.hpp
+++ demo/src/register.hpp
@@ -10,37 +10,33 @@ template<RegisterType reg_type, uintptr_t Address, AccessType access_type=Access
 class Register {
 public:
     constexpr Register() noexcept = default;
+    
     void operator=(reg_type bit_mask) {
         static_assert_writable<access_type>();
-        auto raw_ptr = reinterpret_cast<volatile reg_type*>(Address);
         *raw_ptr = bit_mask;
     }
 
     operator reg_type() const {
         static_assert_readable<access_type>();
-        auto raw_ptr = reinterpret_cast<volatile reg_type*>(Address);
         return *raw_ptr;
     }
 
     void operator |= (reg_type bit_mask) const {
         static_assert_modifiable<access_type>();
-        auto raw_ptr = reinterpret_cast<volatile reg_type*>(Address);
         *raw_ptr |= bit_mask;
     }
 
     void operator &= (reg_type bit_mask) const {
         static_assert_modifiable<access_type>();
-        auto raw_ptr = reinterpret_cast<volatile reg_type*>(Address);
         *raw_ptr &= bit_mask;
     }
 
     void operator ^= (reg_type bit_mask) const {
         static_assert_modifiable<access_type>();
-        auto raw_ptr = reinterpret_cast<volatile reg_type*>(Address);
         *raw_ptr ^= bit_mask;
     } 
 private:
-//    static constexpr volatile reg_type* raw_ptr = reinterpret_cast<volatile reg_type*>(Address);
+    static inline volatile reg_type* const raw_ptr = reinterpret_cast<volatile reg_type*>(Address);
 };
 
 
