9019b7b3caf54ddb19a1453811873f385363bfc9
Aleksander Ferkingstad <Aleksander@alphatek.no>
2025-04-12 22:57:08 +0200

    replace static assert with concepts logic

--- demo/src/register.hpp
+++ demo/src/register.hpp
@@ -4,35 +4,34 @@
 #include <concepts>
 #include <type_traits>
 #include <bit>
-#include "register_traits.hpp"
+
+enum class AccessType {ReadOnly, WriteOnly, ReadWrite};
+
+template<typename T>
+concept RegisterType = std::same_as<T, uint8_t> || std::same_as<T, uint16_t> || std::same_as<T, uint32_t> || std::same_as<T, uint64_t>;
 
 template<RegisterType reg_type, uintptr_t Address, AccessType access_type=AccessType::ReadWrite>
 class Register {
 public:
     constexpr Register() noexcept = default;
     
-    void operator=(reg_type bit_mask) {
-        static_assert_writable<access_type>();
+    void operator=(reg_type bit_mask) const requires(access_type != AccessType::ReadOnly) {
         *raw_ptr = bit_mask;
     }
 
-    operator reg_type() const {
-        static_assert_readable<access_type>();
+    operator reg_type() const requires(access_type != AccessType::WriteOnly){
         return *raw_ptr;
     }
 
-    void operator |= (reg_type bit_mask) const {
-        static_assert_modifiable<access_type>();
+    void operator |= (reg_type bit_mask) const requires(access_type == AccessType::ReadWrite){
         *raw_ptr |= bit_mask;
     }
 
-    void operator &= (reg_type bit_mask) const {
-        static_assert_modifiable<access_type>();
+    void operator &= (reg_type bit_mask) const requires(access_type == AccessType::ReadWrite){
         *raw_ptr &= bit_mask;
     }
 
-    void operator ^= (reg_type bit_mask) const {
-        static_assert_modifiable<access_type>();
+    void operator ^= (reg_type bit_mask) const requires(access_type == AccessType::ReadWrite) {
         *raw_ptr ^= bit_mask;
     } 
 private:
