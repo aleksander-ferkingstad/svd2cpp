80dcb1dfb04518dcfaa279d03520dd55fe739f02
Aleksander Ferkingstad <aleksander@alphatek.no>
2025-04-13 15:36:54 +0200

    minor cleanup

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -2,26 +2,21 @@
 #include "delay.hpp"
 
 constexpr Register<uint32_t, 0x40023830, AccessType::ReadWrite> AHB1ENR;
-constexpr Register<uint32_t, 0x40020400, AccessType::ReadWrite> gpiob_moder;
-constexpr Register<uint32_t, 0x40020414, AccessType::ReadWrite> gpiob_odr;
-/*
-int main() {
-    AHB1ENR.bit<1>() = 1; // Enable GPIOB clock
-    gpiob_moder.slice<0,1>() = 1; // Set MODER bits for PB0 to output mode
-    while(1) {
-        //gpiob_odr.bit<0>() = !gpiob_odr.bit<0>(); // Toggle PB0
-        gpiob_odr ^= 1; // Toggle PB0
-        delay<100000>();
-    }
-}
-*/
+
+struct GPIOB_t {
+    static constexpr uint32_t BASE = 0x40020400;
+    static constexpr auto MODER = Register<uint32_t, BASE, AccessType::ReadWrite>();
+    static constexpr auto ODR = Register<uint32_t, BASE+0x14, AccessType::ReadWrite>();
+};
+
+inline constexpr GPIOB_t GPIOB;
 
 int main() {
-    AHB1ENR |= 3; // Enable GPIOB clock
-    gpiob_moder &= ~(3 << 0); // Clear MODER bits for PB0
-    gpiob_moder |= (1 << 0); // Set MODER bits for PB0 to output mode
+    AHB1ENR.bit<1>() = 1;
+    GPIOB.MODER.bits<0,1>() = 0;
+    GPIOB.MODER.bit<0>() = 1;
     while(1) {
-        gpiob_odr ^= (1 << 0); // Toggle PB0
+        GPIOB.ODR ^= 1;
         delay<100000>();
     }
 }
